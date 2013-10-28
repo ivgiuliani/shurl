3648f0d5e09332b77b202268bc785c3cfad673f5
Ivan Giuliani <giuliani.v@gmail.com>
2013-10-28 18:29:40 +0100

    Include click count for each shortened url

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ drop table if exists entries;
 create table entries (
   slug text primary key,
   url text not null,
+  click_count integer default 0,
   timestamp datetime default CURRENT_TIMESTAMP
 );
